121f8cd18b72428adf067839dfd636c3218550f8
tkingless <t.kingless@yahoo.com>
2016-06-23 15:02:29 +0800

    add mongodb helloworld

---
+++ src/com/tkk/mongodb/MongoDBHelloWorld.java
@@ -0,0 +1,37 @@
+package com.tkk.mongodb;
+import com.mongodb.*;
+import java.net.UnknownHostException;
+
+/**
+ * Created by tkingkwun on 23/6/2016.
+ */
+public class MongoDBHelloWorld {
+    public static void main(String[] args) throws UnknownHostException {
+        //
+        // Creates a new instance of MongoDBClient and connect to localhost
+        // port 27017.
+        //
+        MongoClient client = new MongoClient(
+                new ServerAddress("192.168.33.11", 27017));
+
+        //
+        // Gets the peopledb from the MongoDB instance.
+        //
+        DB database = client.getDB("peopledb");
+
+        //
+        // Gets the persons collections from the database.
+        //
+        DBCollection collection = database.getCollection("users");
+
+        //
+        // Gets a single document / object from this collection.
+        //
+        DBObject document = collection.findOne();
+
+        //
+        // Prints out the document.
+        //
+        System.out.println(document);
+    }
+}
