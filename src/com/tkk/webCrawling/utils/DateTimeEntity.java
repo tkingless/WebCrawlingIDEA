2baf673779179f1a1b0718cf625c0ab76752151f
tkingless <unstashworld@gmail.com>
2016-06-26 00:01:47 +0800

    Declare functions of DateTimeEntity

--- src/com/tkk/webCrawling/utils/DateTimeEntity.java
+++ src/com/tkk/webCrawling/utils/DateTimeEntity.java
@@ -1,7 +1,46 @@
 package com.tkk.webCrawling.utils;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by tkingless on 6/25/16.
  */
 public class DateTimeEntity {
+
+    SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy-MM-dd");
+    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+    Date day;
+    Date time;
+    TimeZone timezone;
+
+    public static Date GetCurrentTime() {
+        return null;
+    }
+
+    public static Date GetToday() {
+        return null;
+    }
+
+    public DateTimeEntity(SimpleDateFormat dayFormat, SimpleDateFormat timeFormat) {
+        this.dayFormat = dayFormat;
+        this.timeFormat = timeFormat;
+    }
+
+    public DateTimeEntity() {
+    }
+
+    public void SetDayFormat(String format) {
+
+    }
+
+    public void SetTimeFormat(String format) {
+
+    }
+
+    public Time CalTimeIntervalDiff (Time time){
+        return  new Time(90);
+    }
 }
