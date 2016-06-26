bbef1a797400188d90b23e782aa04ad09dcc661e
tkingless <unstashworld@gmail.com>
2016-06-26 22:28:22 +0800

    add Match crawlee and workers

---
+++ src/com/tkk/webCrawling/crawleeClass/MatchCrawlee.java
@@ -0,0 +1,7 @@
+package com.tkk.webCrawling.crawleeClass;
+
+/**
+ * Created by tkingless on 6/26/16.
+ */
+public class MatchCrawlee {
+}
---
+++ src/com/tkk/webCrawling/webCrawler/MatchEventWorker.java
@@ -0,0 +1,7 @@
+package com.tkk.webCrawling.webCrawler;
+
+/**
+ * Created by tkingless on 6/26/16.
+ */
+public class MatchEventWorker {
+}
