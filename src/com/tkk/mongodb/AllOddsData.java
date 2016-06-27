05ec249beab264d333877fdecd8d4159aea64124
tsangkk <t.kingless@yahoo.com>
2016-06-27 13:49:15 +0800

    added new classes: allodds and dbmanager

---
+++ src/com/tkk/mongodb/AllOddsData.java
@@ -0,0 +1,7 @@
+package com.tkk.mongodb;
+
+/**
+ * Created by tsangkk on 6/27/16.
+ */
+public class AllOddsData {
+}
---
+++ src/com/tkk/mongodb/DBManager.java
@@ -0,0 +1,15 @@
+package com.tkk.mongodb;
+
+/**
+ * Created by tsangkk on 6/27/16.
+ */
+public class DBManager {
+    private static DBManager ourInstance = new DBManager();
+
+    public static DBManager getInstance() {
+        return ourInstance;
+    }
+
+    private DBManager() {
+    }
+}
