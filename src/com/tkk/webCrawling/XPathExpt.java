c94f704759585416af8e93e9d2e207f81fcd2716
tkingless <unstashworld@gmail.com>
2016-07-01 18:05:22 +0800

    added invalid code

--- src/com/tkk/webCrawling/XPathExpt.java
+++ src/com/tkk/webCrawling/XPathExpt.java
@@ -1,11 +1,38 @@
 package com.tkk.webCrawling;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by tkingless on 7/1/16.
  */
 public class XPathExpt {
 
-    public void static main () {
+    public static void main(String[] args) {
+
+        String uri = "http://bet.hkjc.com/football/getXML.aspx?pooltype=all&isLiveBetting=true&match=103867";
+
+       /* URL url = new URL(uri);
+        HttpURLConnection connection =
+                (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/xml");
+
+        InputStream xml = connection.getInputStream();
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(xml);
+
+        System.out.println(doc.toString());*/
 
     }
 }
