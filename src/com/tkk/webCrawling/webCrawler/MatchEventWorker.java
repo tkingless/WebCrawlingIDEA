c06659dc3a4a362e27d63748815e8c76d14e689d
tkingless <unstashworld@gmail.com>
2016-07-03 23:14:28 +0800

    update worker

--- src/com/tkk/webCrawling/webCrawler/MatchEventWorker.java
+++ src/com/tkk/webCrawling/webCrawler/MatchEventWorker.java
@@ -18,11 +18,19 @@ public class MatchEventWorker extends baseCrawler {
 
     enum MatchState {
         STATE_INITIALIZATION,
+        STATE_FUTURE_MATCH,
         //only at state of pre_registered, the thread is launched
         STATE_PRE_REGISTERED,
         STATE_MATCH_START,
         STATE_MATCH_LOGGING,
         STATE_MATCH_ENDED
+    }
+
+    enum MatchStage {
+        STAGE_ESST,
+        STAGE_FIRST,
+        STAGE_HALFTIME,
+        STAGE_SECOND,
     }
 
     static final String threadName = "MatchEventWorker-thread";
@@ -31,16 +39,18 @@ public class MatchEventWorker extends baseCrawler {
     //The unique id for this worker
     String matchId;
     long scanPeriod = 5000;
+    long preRegperiod = 1000 * 60 * 5;
 
     DateTimeEntity commenceTime;
     MatchState status;
+    MatchStage stage;
     //the secondary key to be used
     String matchKey;
     String matchTeams;
 
 
     public MatchEventWorker(String aMatchId, Element matchKeyEle, Element statusEle, Element teamsEle) {
-        super(CrawlerKeyBinding.MatchEvent, threadName+"-"+aMatchId);
+        super(CrawlerKeyBinding.MatchEvent, threadName + "-" + aMatchId);
         status = MatchState.STATE_INITIALIZATION;
         matchId = aMatchId;
         System.out.println("MatchEventWorker constructed, matchId:" + matchId);
@@ -60,7 +70,7 @@ public class MatchEventWorker extends baseCrawler {
         this.StartRun();
     }
 
-    void ExtractMatcdKey (Element matchKeyEle) {
+    void ExtractMatcdKey(Element matchKeyEle) {
         matchKey = matchKeyEle.text();
         System.out.println("GetChildNodes(), matchKey: " + matchKey);
     }
@@ -87,6 +97,16 @@ public class MatchEventWorker extends baseCrawler {
 
             commenceTime = new DateTimeEntity(dateTimeBuilder.toString(), new SimpleDateFormat("HH:mm:ss dd/MM/yyyy"));
             System.out.println("ExtractStatus(),commenceTime: " + commenceTime.toString());
+            stage = MatchStage.STAGE_ESST;
+        }else if (statusEle.text().contains("1st Half In Progress")) {
+            //TODO something
+            stage = MatchStage.STAGE_FIRST;
+        }else if (statusEle.text().contains("Half Time")) {
+            //TODO something
+            stage = MatchStage.STAGE_HALFTIME;
+        }else if (statusEle.text().contains("2nd Half In Progress")){
+            //TODO something
+            stage = MatchStage.STAGE_SECOND;
         }
     }
 
@@ -95,20 +115,16 @@ public class MatchEventWorker extends baseCrawler {
         System.out.println("GetChildNodes(), matchTeams: " + matchTeams);
     }
 
-    public boolean IsLive(){
+    public boolean IsLive() {
         //TODO
         return false;
     }
 
-    public void LaunchProcess(){
+    public void LaunchProcess() {
         //TODO, if fullfill criteria, start a monitoring thread
     }
 
-    public void SetupParam(){
-
-    }
-
-    void EmitRequest () {
+    void EmitRequest() {
         //TODO grab data periodically
     }
 
@@ -123,27 +139,53 @@ public class MatchEventWorker extends baseCrawler {
 
 
     //The main loop function
-    void Proc(){
-        int count = 0;
-        while (status != MatchState.STATE_MATCH_ENDED){
-            //TODO doing something appropriate
+    void Proc() {
 
-            if (count > 2)
-                break;
+        while (status != MatchState.STATE_MATCH_ENDED || status != MatchState.STATE_FUTURE_MATCH) {
 
-            count++;
+            if (status == MatchState.STATE_INITIALIZATION) {
+                //TODO
+                //compare the match startime and check the match status,
+                //////check if Match_stage==InPlayESST_nobr
+                ////////if too long away from statrtime, set state to STATE_FUTURE_MATCH
+                ////////else if within the preRegperiod, enter STATE_PRE_REGISTERED
+                //if already in progress and no record before, set the startime to now, and state to STATE_PRE_REGISTERED
+                System.out.println("Threadname: " + threadName + matchId + " STATE_INITIALIZATION");
 
-            System.out.println("Threadname: " + threadName + matchId + " , run() count is: " + count);
 
-            try {
+                long timediff = commenceTime.CalTimeIntervalDiff(new DateTimeEntity());
+                System.out.println("timediff: " + timediff + " id: " + matchId);
+                status = MatchState.STATE_PRE_REGISTERED;
+            } else if (status == MatchState.STATE_PRE_REGISTERED) {
+                //TODO
+                //check if the match qualified has been registered, ensure the registration of the match (about to start/already started) to DB and the static liveworkers
+                //////if yes (def: both DB and local instance of boardcrawlee has record), set state to STATE_MATCH_START
+                //////if no, register. Ensure boardcrawlee will not add same instance
+                //note here is thread safe concern about static liveworker on class BoardCrawlee
+                //
+                //listen to the allodds xml, wait the MATCH_STAGE to "firsthalf", then
+                System.out.println("Threadname: " + threadName + matchId + " STATE_PRE_REGISTERED");
+                status = MatchState.STATE_MATCH_ENDED;
+            } else if (status == MatchState.STATE_MATCH_START) {
+
+
+                System.out.println("Threadname: " + threadName + matchId + " STATE_MATCH_START");
+            } else if (status == MatchState.STATE_FUTURE_MATCH){
+                //TODO add the match registration to DB
+            }
+
+
+            //System.out.println("Threadname: " + threadName + matchId);
+
+           /* try {
                 sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+            }*/
         }
     }
 
-    public void Kill(){
+    public void Kill() {
         //TODO: make sure detached from any pointing, and thread on this object is stopped()
     }
 }
