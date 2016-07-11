d80839b5ce2cf1d9107c67ab2d776a28eb8f4183
tsangkk <t.kingless@yahoo.com>
2016-07-11 17:18:33 +0800

    add workerTest

---
+++ src/com/tkk/webCrawling/webCrawler/MatchEventWorkerTest.java
@@ -0,0 +1,22 @@
+package com.tkk.webCrawling.webCrawler;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tsangkk on 7/11/16.
+ */
+public class MatchEventWorkerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
