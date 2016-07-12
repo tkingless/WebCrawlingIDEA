a67b6f2dcf303bf0ee00584d7d6275e4471a8647
tsangkk <t.kingless@yahoo.com>
2016-07-12 17:17:41 +0800

    add PreRegWorkerTest class

---
+++ src/com/tkk/webCrawling/webCrawler/PreRegWorkerTest.java
@@ -0,0 +1,22 @@
+package com.tkk.webCrawling.webCrawler;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tsangkk on 7/12/16.
+ */
+public class PreRegWorkerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
