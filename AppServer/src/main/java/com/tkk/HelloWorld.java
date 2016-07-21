df9c2d52d4c3faaef99469ec6dd2e196c463a8f5
tkingless <unstashworld@gmail.com>
2016-07-21 23:00:52 +0800

    can init an object on Tomcat start

--- AppServer/src/main/java/com/tkk/HelloWorld.java
+++ AppServer/src/main/java/com/tkk/HelloWorld.java
@@ -4,6 +4,7 @@ import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
 
 @WebServlet(urlPatterns={"/hello"})
 public class HelloWorld extends HttpServlet {
@@ -19,6 +20,13 @@ public class HelloWorld extends HttpServlet {
     //TODO how appserver connect to WebCrawling
 
     //TODO make a class of Appeserver can run the codes of WebCrawling when Tomcat startup, and the hooking worker properly shutdown when server down
+    public void init() throws ServletException
+    {
+        System.out.println("----------");
+        System.out.println("---------- CrunchifyServletExample Initialized successfully ----------");
+        System.out.println("----------");
+        //TODO do something meaningful
+    }
 
     //REF: http://crunchify.com/how-to-start-embedded-http-jersey-server-during-java-application-startup/
     //REF: http://crunchify.com/how-to-run-java-program-automatically-on-tomcat-startup/
--- AppServer/src/main/webapp/hello.jsp
+++ AppServer/src/main/webapp/hello.jsp
@@ -1,5 +1,6 @@
 <html>
     <body>
-        <h1>Hello, ${user}</h1>
+    <!--    <h1>Hello, ${user}</h1> -->
+     <h1>Hello, the service should be running</h1>
     </body>
 </html>
