7f203dbf915f545b64c6b7e8fe25a0e479842bfc
tsangkk <t.kingless@yahoo.com>
2016-07-22 09:43:06 +0800

    just a mess

--- AppServer/src/main/java/com/tkk/HelloWorld.java
+++ AppServer/src/main/java/com/tkk/HelloWorld.java
@@ -1,12 +1,13 @@
 package com.tkk;
 
 import java.io.*;
+import javax.annotation.PreDestroy;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet(urlPatterns={"/hello"})
+@WebServlet(urlPatterns = {"/hello"})
 public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -17,15 +18,33 @@ public class HelloWorld extends HttpServlet {
                 .forward(req, resp);
     }
 
-    //TODO how appserver connect to WebCrawling
-
     //TODO make a class of Appeserver can run the codes of WebCrawling when Tomcat startup, and the hooking worker properly shutdown when server down
-    public void init() throws ServletException
-    {
+    @Override
+    public void init() throws ServletException {
+        super.init();
         System.out.println("----------");
-        System.out.println("---------- CrunchifyServletExample Initialized successfully ----------");
+        System.out.println("---------- HelloWorld servlet Initialized successfully ----------");
         System.out.println("----------");
         //TODO do something meaningful
+
+        //TODO get System.out to a log file
+    }
+
+    @PreDestroy
+    public void OnDestroy() {
+        System.out.println("----------");
+        System.out.println("---------- HelloWorld servlet destroyed ----------");
+        System.out.println("----------");
+
+        /*
+          log.info("Signaling worker to stop current job/not begin another.");
+  this.worker.stop();
+  while(this.worker.isProcessingJob()) {
+    Thread.sleep(3 * 1000);
+  }
+  log.info("Worker successfully stopped");
+  log.info("destroy() complete");
+         */
     }
 
     //REF: http://crunchify.com/how-to-start-embedded-http-jersey-server-during-java-application-startup/
