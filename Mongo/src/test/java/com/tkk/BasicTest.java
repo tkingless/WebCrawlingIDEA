44e223b589c2ec014f8c5c38349e8aed8bc6a674
tsangkk <t.kingless@yahoo.com>
2016-07-26 15:33:37 +0800

    update test cases

--- Mongo/src/test/java/com/tkk/BasicTest.java
+++ Mongo/src/test/java/com/tkk/BasicTest.java
@@ -1,7 +1,28 @@
 package com.tkk;
 
+import org.junit.Assert;
+import org.junit.Test;
+import com.mongodb.*;
+
 /**
  * Created by tsangkk on 7/26/16.
  */
 public class BasicTest {
+
+    String DBaddr = "127.0.0.1";
+    int DBport = 27017;
+
+    @Test
+    public void TestConnection () throws Exception {
+        MongoClientOptions.Builder o = MongoClientOptions.builder().connectTimeout(3000);
+        MongoClient client = new MongoClient(new ServerAddress(DBaddr,DBport));
+
+        try {
+            client.getAddress();
+        } catch (Exception e) {
+            System.out.println("Mongo is down");
+            client.close();
+            return;
+        }
+    }
 }
