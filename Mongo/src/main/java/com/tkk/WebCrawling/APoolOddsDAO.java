e29894d112e9b0e36557942039bfa12442160677
tkingless <unstashworld@gmail.com>
2016-07-29 09:39:32 +0800

    update data model

--- Mongo/src/main/java/com/tkk/WebCrawling/APoolOddsDAO.java
+++ Mongo/src/main/java/com/tkk/WebCrawling/APoolOddsDAO.java
@@ -5,8 +5,8 @@ import java.util.List;
  * Created by tsangkk on 7/26/16.
  */
 public interface APoolOddsDAO {
-    public List<APoolOdds> getAll_ALLOddsData();
-    public APoolOdds getAllOdds(int matchNo);
-    public void updateAllOdds(APoolOdds odds);
-    public void deleteAllOdds(APoolOdds odds);
+    public List<APoolOddsData> getAll_ALLOddsData();
+    public APoolOddsData getAllOdds(int matchNo);
+    public void updateAllOdds(APoolOddsData odds);
+    public void deleteAllOdds(APoolOddsData odds);
 }
