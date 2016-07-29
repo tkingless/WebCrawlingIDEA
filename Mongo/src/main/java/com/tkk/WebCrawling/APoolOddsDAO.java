9396fb1c834638b3e8ed1c42764d535bf9c7cefa
tkingless <unstashworld@gmail.com>
2016-07-29 09:47:24 +0800

    update classes

--- Mongo/src/main/java/com/tkk/WebCrawling/APoolOddsDAO.java
+++ Mongo/src/main/java/com/tkk/WebCrawling/APoolOddsDAO.java
@@ -4,9 +4,20 @@ import java.util.List;
 /**
  * Created by tsangkk on 7/26/16.
  */
-public interface APoolOddsDAO {
-    public List<APoolOddsData> getAll_ALLOddsData();
-    public APoolOddsData getAllOdds(int matchNo);
-    public void updateAllOdds(APoolOddsData odds);
-    public void deleteAllOdds(APoolOddsData odds);
+
+public class APoolOddsDAO {
+
+
+    /*public List<APoolOddsData> getAll_ALLOddsData(){
+
+    }
+    public APoolOddsData getAllOdds(int matchNo){
+
+    }
+    public void updateAllOdds(APoolOddsData odds){
+
+    }
+    public void deleteAllOdds(APoolOddsData odds){
+
+    }*/
 }
