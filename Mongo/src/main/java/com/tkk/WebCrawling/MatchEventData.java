42bb436b93080ff81fecdf379d35bc465aff8d64
tkingless <unstashworld@gmail.com>
2016-07-29 23:38:33 +0800

    update data

--- Mongo/src/main/java/com/tkk/WebCrawling/MatchEventData.java
+++ Mongo/src/main/java/com/tkk/WebCrawling/MatchEventData.java
@@ -26,17 +26,17 @@ public class MatchEventData {
     private int MatchId;
     private String MatchKey;
     private HashMap<Date,String> stageUpdates;
+    private HashMap<Date,String> scoreUpdate;
+    private HashMap<Date,Integer> cornerTotUpdate;
 
     private String homeTeam;
     private String awayTeam;
     private List<String> poolTypes;
-
 
     private Date commence;
     private Date actualCommence;
     private Date endTime;
     private Date createdAt;
     private Date lastModifiedAt;
-
 
 }
