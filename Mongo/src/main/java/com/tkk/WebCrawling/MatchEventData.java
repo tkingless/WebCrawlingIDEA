4896cf07fddeaea4735cd1cb120e0c77f2221bfd
tkingless <unstashworld@gmail.com>
2016-07-29 21:38:31 +0800

    update Data model

--- Mongo/src/main/java/com/tkk/WebCrawling/MatchEventData.java
+++ Mongo/src/main/java/com/tkk/WebCrawling/MatchEventData.java
@@ -1,7 +1,42 @@
 package com.tkk.WebCrawling;
 
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.*;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.mongodb.morphia.utils.IndexType.DESC;
+
 /**
  * Created by tkingless on 7/29/16.
  */
+
+@Entity("MatchEvents")
+@Indexes({
+        @Index(value = "MatchId", fields = @Field(value = "MatchdId", type = DESC)),
+        @Index(value = "createdAt", fields = @Field(value = "createdAt", type = DESC))
+})
+
 public class MatchEventData {
+
+    @Id
+    private ObjectId id;
+    private int MatchId;
+    private String MatchKey;
+    private HashMap<Date,String> stageUpdates;
+
+    private String homeTeam;
+    private String awayTeam;
+    private List<String> poolTypes;
+
+
+    private Date commence;
+    private Date actualCommence;
+    private Date endTime;
+    private Date createdAt;
+    private Date lastModifiedAt;
+
+
 }
