6c7fae6b91d973b24cfad4331999f7e114347e26
tsangkk <t.kingless@yahoo.com>
2016-08-03 10:40:46 +0800

    update sorting example

--- WebCrawling/src/test/java/com/tkingless/Miscs.java
+++ WebCrawling/src/test/java/com/tkingless/Miscs.java
@@ -1,6 +1,12 @@
 package com.tkingless;
 
+import com.tkingless.WebCrawling.DBobject.APoolOddsData;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by tkingless on 7/30/16.
@@ -15,5 +21,32 @@ public class Miscs {
         for(String part: parts){
            System.out.println(part);
         }
+    }
+
+    @Test
+    public void TestDateSortedList() throws Exception {
+        List<APoolOddsData> sortedList = new ArrayList<>();
+
+        sortedList.add(new APoolOddsData(new Date((long)1470190376*1000)));
+        sortedList.add(new APoolOddsData(new Date((long)(1470190376+1000)*1000)));
+        sortedList.add(new APoolOddsData(new Date((long)(1470190376-1000)*1000)));
+
+        for(APoolOddsData data : sortedList){
+            System.out.println(data.getRecorded());
+        }
+
+        //ASC ORDER
+        //sortedList.sort(Comparator.comparing (data -> data.getRecorded()));
+
+        //DESC ORDER
+        Comparator<APoolOddsData> comparator = (h1, h2) -> h1.getRecorded().compareTo(h2.getRecorded());
+        sortedList.sort(comparator.reversed());
+
+        for(APoolOddsData data : sortedList){
+            System.out.println(data.getRecorded());
+        }
+
+
+
     }
 }
