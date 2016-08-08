498ac605fb2bad916da6791f16f22e60b8eba86e
tsangkk <t.kingless@yahoo.com>
2016-08-08 16:00:38 +0800

    update WCDIO

--- AppServer/src/main/java/com/tkingless/WCDIOconstants.java
+++ AppServer/src/main/java/com/tkingless/WCDIOconstants.java
@@ -4,4 +4,6 @@ package com.tkingless;
  * Created by tsangkk on 8/8/16.
  */
 public class WCDIOconstants {
+
+    public final static String testEnvFileSerlvetABSpath = "/home/tsangkk/Development/Github/WebCrawlingIDEA/AppServer/HostedFiles";
 }
