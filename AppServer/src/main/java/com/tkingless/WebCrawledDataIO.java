4ec702a08e36e1d5c28b89307b0b5e386bb32bf8
tsangkk <t.kingless@yahoo.com>
2016-08-08 16:00:52 +0800

    added tasklist

--- AppServer/src/main/java/com/tkingless/WebCrawledDataIO.java
+++ AppServer/src/main/java/com/tkingless/WebCrawledDataIO.java
@@ -13,6 +13,13 @@ public class WebCrawledDataIO implements ServletContextListener {
 
     final static Logger logger = LogManager.getLogger(WebCrawledDataIO.class);
 
+    //TODO can write a file into the filesharing folder
+    //TODO can connect to DB to get data
+    //TODO can call CSV, file manager
+    //TODO can have working threads, again....my gosh
+
+    //ref: http://balusc.omnifaces.org/2007/07/fileservlet.html
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
 
