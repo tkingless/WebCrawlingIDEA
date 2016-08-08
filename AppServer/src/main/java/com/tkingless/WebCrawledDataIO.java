c4098446aa391c6ed1e521e1de52eb450f8e6b8c
tsangkk <t.kingless@yahoo.com>
2016-08-08 15:32:27 +0800

    update WebCrawledDataIO

--- AppServer/src/main/java/com/tkingless/WebCrawledDataIO.java
+++ AppServer/src/main/java/com/tkingless/WebCrawledDataIO.java
@@ -1,7 +1,25 @@
 package com.tkingless;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 /**
  * Created by tsangkk on 8/8/16.
  */
-public class WebCrawledDataIO {
+public class WebCrawledDataIO implements ServletContextListener {
+
+    final static Logger logger = LogManager.getLogger(WebCrawledDataIO.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
 }
