d08bfb8bf9070a738a00750320620432e3179e8f
tsangkk <t.kingless@yahoo.com>
2016-08-10 09:59:19 +0800

    deleted constatns java

--- AppServer/src/main/java/com/tkingless/WCDIOconstants.java
+++
@@ -1,9 +0,0 @@
-package com.tkingless;
-
-/**
- * Created by tsangkk on 8/8/16.
- */
-public class WCDIOconstants {
-
-    public final static String testEnvFileSerlvetABSpath = "/home/tkingless/Development/Github/WebCrawlingIDEA/AppServer/HostedFiles";
-}
