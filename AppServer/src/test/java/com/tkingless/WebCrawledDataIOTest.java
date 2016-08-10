da33d29413c7942783ab1865c88baa896e780515
tsangkk <t.kingless@yahoo.com>
2016-08-10 11:17:53 +0800

    update tests and gitignore

--- AppServer/src/test/java/com/tkingless/WebCrawledDataIOTest.java
+++ AppServer/src/test/java/com/tkingless/WebCrawledDataIOTest.java
@@ -1,7 +1,26 @@
 package com.tkingless;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by tsangkk on 8/10/16.
  */
 public class WebCrawledDataIOTest {
+
+
+    @Before
+    public void init() throws Exception {
+
+    }
+
+    @Test
+    public void exportDBcollections() throws Exception{
+
+    }
+
+    @Test
+    public void importDBcollections() throws Exception{
+
+    }
 }
