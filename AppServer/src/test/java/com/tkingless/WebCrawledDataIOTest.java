a5f77f40e3773610e1090f10efb3e9534355aaa4
tsangkk <t.kingless@yahoo.com>
2016-08-12 15:27:45 +0800

    update tests

--- AppServer/src/test/java/com/tkingless/WebCrawledDataIOTest.java
+++ AppServer/src/test/java/com/tkingless/WebCrawledDataIOTest.java
@@ -1,14 +1,20 @@
 package com.tkingless;
 
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.ListCollectionsIterable;
+import com.mongodb.client.MongoDatabase;
+import com.tkingless.utils.DateTimeEntity;
+import org.bson.Document;
 import org.junit.Before;
 import org.junit.Test;
 
-import static com.tkingless.MongoDBparam.TestDBAddr;
-import static com.tkingless.MongoDBparam.TestDBport;
-import static com.tkingless.MongoDBparam.webCrawlingTestDB;
+import java.util.*;
+
+import static com.tkingless.MongoDBparam.*;
 
 /**
  * Created by tsangkk on 8/10/16.
@@ -22,13 +28,16 @@ public class WebCrawledDataIOTest {
 
     MongoClient client;
 
+    MongoDatabase tmpProdDB;
+
     @Before
     public void init() throws Exception {
         MongoClientOptions.Builder o = MongoClientOptions.builder().connectTimeout(3000);
-        client = new MongoClient(new ServerAddress(DBaddr,DBport));
+        client = new MongoClient(new ServerAddress(DBaddr, DBport));
 
         try {
             client.getAddress();
+            tmpProdDB = client.getDatabase(webCrawlingDB);
         } catch (Exception e) {
             System.out.println("Mongo is down");
             client.close();
@@ -37,12 +46,55 @@ public class WebCrawledDataIOTest {
     }
 
     @Test
-    public void exportDBcollections() throws Exception{
+    public void GetConsideredWorkersByTime() throws Exception {
+
+        long threshold = (new Date()).getTime() - 1000 * 60 * 60 * 72;
+        DateTimeEntity timeAfterToConsider = new DateTimeEntity(threshold);
+
+        List<Integer> onMatchingIds = new ArrayList<>();
+        List<Integer> lostMatchingIds = new ArrayList<>();
+
+        FindIterable<Document> consideredIds = tmpProdDB.getCollection("MatchEvents").find(
+                //Multiple criteria
+                new Document("actualCommence", new Document("$exists", true)).append(
+                        "actualCommence", new Document("$gte", timeAfterToConsider.GetTheInstant())).append(
+                        "scoreUpdates", new Document("$exists", true)).append(
+                        "stageUpdates", new Document("$exists", true))
+        ).projection(new Document("MatchId", 1).append("scoreUpdates", 1).append("stageUpdates", 1).append("actualCommence", 1).append("endTime", 1))
+                .sort(new Document("actualCommence",-1))
+                .limit(20);
+
+        consideredIds.forEach(new Block<Document>() {
+            @Override
+            public void apply(final Document document) {
+                System.out.println(document.toJson());
+                //if(document.keySet().containsAll(Arrays.asList("scoreUpdates","stageUpdates"))){
+
+                try {
+
+                    Date docCommenceDate = (Date) document.get("actualCommence");
+                    DateTimeEntity docCommence = new DateTimeEntity(docCommenceDate.getTime());
+
+                    if (docCommence.CalTimeIntervalDiff(timeAfterToConsider) >= 0) {
+                        onMatchingIds.add(document.getInteger("MatchId"));
+                    } else {
+                        lostMatchingIds.add(document.getInteger("MatchId"));
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        System.out.println("onMatchingIds : " + onMatchingIds.toString());
+        System.out.println("lostMatchingIds : " + lostMatchingIds.toString());
 
     }
 
     @Test
-    public void importDBcollections() throws Exception{
+    public void importDBcollections() throws Exception {
 
     }
 }
