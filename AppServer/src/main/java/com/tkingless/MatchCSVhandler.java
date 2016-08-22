7bea698edc7627eeb79978e4e7badd35127c3a06
tsangkk <t.kingless@yahoo.com>
2016-08-22 15:05:00 +0800

    update matchCSVhandler

--- AppServer/src/main/java/com/tkingless/MatchCSVhandler.java
+++ AppServer/src/main/java/com/tkingless/MatchCSVhandler.java
@@ -13,10 +13,38 @@ public class MatchCSVhandler {
     String csvTeamA;
 
     String subFolder;
-    DateTimeEntity folderDate;
 
-    public MatchCSVhandler(Document WCDIO, String homeTeam, String awayTeam){
-        //TODO need to know WCDIOdoc, id, matchteams
+    String rootPath;
+
+    Document WCDIO;
+
+    private boolean lastOutSucceed = false;
+
+    //TODO look into WCDIOcsv, look for documents that has not ended writing, defined as !(MarkedEnded&&lastOut>=lastIn), !lastOut:exists
+    //TODO the subfolder format is 22082016
+
+    public MatchCSVhandler(Document WCDIO, String homeTeam, String awayTeam, String root){
+        rootPath = root;
+        csvTeamH = homeTeam;
+        csvTeamA = awayTeam;
+        csvTeamH = csvTeamH.replace(' ', '_');
+        csvTeamA = csvTeamA.replace(' ', '_');
+
+        csvFile = WCDIO.getInteger("MatchId").toString() + '.' + csvTeamH + '.' + csvTeamA + ".csv";
+
+        subFolder=DateTimeEntity.GetToday();
+
+        this.WCDIO = WCDIO;
+
+
+    }
+
+    public void run() {
+
+    }
+
+    public boolean isLastOutSucceed() {
+        return lastOutSucceed;
     }
 
     private void Overwrite(){
@@ -25,6 +53,28 @@ public class MatchCSVhandler {
 
     private void Append(){
 
+    }
+
+    @Override
+    public String toString(){
+        String tmp = null;
+
+        tmp += "csvTeamH: " + csvTeamH;
+        tmp += "\ncsvTeamA:" + csvTeamA;
+
+        if(WCDIO != null){
+            if(WCDIO.containsKey("lastIn")){
+                tmp += "\nlastIn: " + WCDIO.getDate("lastIn");
+            }
+            if(WCDIO.containsKey("MarkedEnd")){
+                tmp += "\nMarkedEnd: " + WCDIO.getDate("MarkedEnd");
+            }
+            if(WCDIO.containsKey("lastOut")){
+                tmp += "\nlastOut: " + WCDIO.getDate("lastOut");
+            }
+        }
+
+        return tmp;
     }
 
 }
