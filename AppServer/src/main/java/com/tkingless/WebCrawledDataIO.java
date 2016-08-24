e06ae32ab29df4a469522fcfe2cac23f586bfca8
tkingless <t.kingless@yahoo.com>
2016-08-24 10:50:38 +0800

    update WebCrawledDataIO

--- AppServer/src/main/java/com/tkingless/WebCrawledDataIO.java
+++ AppServer/src/main/java/com/tkingless/WebCrawledDataIO.java
@@ -1,16 +1,10 @@
 package com.tkingless;
 
-import com.tkingless.utils.FileManager;
-import com.tkingless.webCrawler.HKJCcrawler;
-import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bson.Document;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -23,23 +17,24 @@ public class WebCrawledDataIO implements ServletContextListener {
 
     final static Logger logger = LogManager.getLogger(WebCrawledDataIO.class);
 
-    private String filePath;
-    private ScheduledExecutorService scheduler;
+    private ScheduledExecutorService Inscheduler;
+    private ScheduledExecutorService Outscheduler;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         logger.info("WebCrawledDataIO init() called.");
 
-        //TODO csvout worker should delay sometime after csvin worker, sensibly, and lower frequency
-        scheduler = Executors.newSingleThreadScheduledExecutor();
-        scheduler.scheduleAtFixedRate(new ScheduledWCDIOin(),0,1000 * 5, TimeUnit.MILLISECONDS);
+        Inscheduler = Executors.newSingleThreadScheduledExecutor();
+        Inscheduler.scheduleAtFixedRate(new ScheduledWCDIOin(),0,1000 * 5, TimeUnit.MILLISECONDS);
+        Outscheduler = Executors.newSingleThreadScheduledExecutor();
+        Outscheduler.scheduleAtFixedRate(new ScheduledWCDIOout(),2500,1000 * 6, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
 
         try {
-            scheduler.shutdown();
+            Inscheduler.shutdown();
             Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
